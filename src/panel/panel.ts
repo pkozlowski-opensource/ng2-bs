c834884c4aa7fafe6473ab378eec20945ebeb527
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-09-14 12:07:33 +0200

    docs(panel): document potential improvements for panels

--- src/panel/panel.ts
+++ src/panel/panel.ts
@@ -1,5 +1,6 @@
 import {Component, Directive, Query, QueryList, View, NgIf} from 'angular2/angular2';
 
+//TODO: why can't I query for regular elements?
 @Directive({selector: 'bs-panel-heading'})
 export class BsPanelHeading {}
 
@@ -18,10 +19,13 @@ export class BsPanel {
     panelTitle: string;
     type: string;
     collapsed: boolean = false;
+    //TODO: why @Query isn't enough and I have to use type annotation as well?
     constructor(@Query(BsPanelHeading) private _headings: QueryList<BsPanelHeading>,
                 @Query(BsPanelFooter) private _footers: QueryList<BsPanelFooter>) {
     }
 
+
+    //TODO: add a `isEmpty()` helper method?
     hasHeadings(): boolean {
         return this._headings.length > 0;
     }
