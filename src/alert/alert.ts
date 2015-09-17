345b23969fdfd16e5d0e462b8e3c8fe312e1cdf6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-09-17 16:48:44 +0200

    refactor(alert): simplify

--- src/alert/alert.ts
+++ src/alert/alert.ts
@@ -10,14 +10,9 @@ import {Component, View, NgIf, EventEmitter} from 'angular2/angular2';
     directives: [NgIf]
 })
 export class BsAlert {
-    static alertTypes = ['success', 'info', 'warning', 'danger'];
     _dismissible: boolean = false;
-    _type: string = BsAlert.alertTypes[2];
+    type: string;
     dismiss: EventEmitter = new EventEmitter();
-
-    set type(val: string) {
-        this._type = BsAlert.alertTypes.indexOf(val) !== -1 ? val : BsAlert.alertTypes[2];
-    }
 
     set dismissible(val: string | boolean) {
         this._dismissible = String(val) == "true";
